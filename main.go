c2f87931989ff1f05636ff62c13f669a58e2ae39
Nick Molnar <iam+git@nicholasmolnar.com>
2018-05-01 14:00:00 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// Comment on line 3
+func someFunction() string {
+	variableA := "a"
+	return variableA
+}
+
+// Comment on line 9``
+func someOtherFunction() string {
+	variableB := "b"
+	return variableB
+}
