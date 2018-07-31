55060e73311b2c80c103e6bcd19d53be914772f3
Levin Dixon <levindixon@gmail.com>
2018-07-31 11:36:25 -0700

    update variable names

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 // Comment on line 3
 func SomeFunction() string {
-	variableX := "X"
-	return variableX
+	variableY := "Y"
+	return variableY
 }
 
 // Comment on line 9``
 func bestDifferentFunction() string {
-	variableY := "changed from y"
-	return variableY
+	variableB := "changed from b"
+	return variableB
 }
